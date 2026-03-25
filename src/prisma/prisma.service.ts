3eb53e3b97fb9b8fe50f64b0411aacf6d249934d
1phanh11 <nguyenhuynhphuanh989@gmail.com>
2026-03-25 11:02:46 +0700

    feat: prisma init and migrate schema from expressjs project

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL});
+        super({ adapter });
+    }
+    async onModuleInit() {
+        this.$connect();
+        console.log("Prisma DB: Connected");
+    }
+    async onModuleDestroy() {
+        this.$disconnect()
+        console.log("Prisma DB: disconected");
+    }
+}
